f9ec9d34a6147f098dad7b538515804bf2a08896
John McMahon <john.mcmahon@outlook.com>
2025-01-31 15:46:13 -0800

    feat(core): upload of the project

---
+++ src/stories/NavbarMenu.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NavbarMenuComponent = void 0;
+var jsx_runtime_1 = require("react/jsx-runtime");
+var NavbarMenuComponent = function (user) {
+    return ((0, jsx_runtime_1.jsx)("button", {}));
+};
+exports.NavbarMenuComponent = NavbarMenuComponent;
